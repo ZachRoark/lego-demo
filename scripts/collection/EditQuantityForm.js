8f8842b0894bc67602dce3496659a6633fc4d7ac
Brenda Long <brenda@studio6thave.com>
2020-09-22 08:45:53 -0500

    clean up extra reference

--- scripts/collection/EditQuantityForm.js
+++ scripts/collection/EditQuantityForm.js
@@ -9,19 +9,19 @@ eventHub.addEventListener("click", event => {
 		// collect the details of brick
 		//id: 1, brickId: 1, quantity: 6, userId: 1
 		const updatedBrick = {
-		brickId: parseInt(document.querySelector(`#edit--APIBrick`).value),
-		userId: getUser().id,
-		quantity: parseInt(document.querySelector(`#input-quantity--${id}`).value)
+			brickId: parseInt(document.querySelector(`#edit--APIBrick`).value),
+			userId: getUser().id,
+			quantity: parseInt(document.querySelector(`#input-quantity--${id}`).value)
 		}
-		const userBricksId = parseInt(document.querySelector("#editQuanity--id").value)
-		editQuantity(updatedBrick, userBricksId)
+		editQuantity(updatedBrick, parseInt(id))
 	}
 })
 export const EditQuantityForm = (userBricksId) => {
+	//get latest userBrick info from DB
 	const brick = getSingleBrick(parseInt(userBricksId))
       .then((response) => {
+		//replace static quantity with inputs
         document.querySelector(`#quantity--${userBricksId}`).innerHTML = `
-          <input type="hidden" value="${userBricksId}" id="editQuanity--id">
           <input type="hidden" value="${response.brickId}" id="edit--APIBrick">
           <input id="input-quantity--${userBricksId}" value="${response.quantity}"/>
           <button id="saveQuantity--${userBricksId}">Save</button>
